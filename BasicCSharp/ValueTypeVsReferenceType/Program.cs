22134fb3dbf84a76877e37429d7c68278273b5a9
raphaellpro <826.xxxxx@gmail.com>
2025-03-19 17:22:47 -0300

    feat: simulation of the Null Exception error

--- BasicCSharp/ValueTypeVsReferenceType/Program.cs
+++ BasicCSharp/ValueTypeVsReferenceType/Program.cs
@@ -11,17 +11,27 @@ namespace ValueTypeVsReferenceType
         static void Main(string[] args)
         {
             // Passagem por valor
-            int numero1 = 3;
+            /*int numero1 = 3;
             int numero2 = numero1;
             numero1 = 5;
             Console.WriteLine("numero1 = {0}, numero2 = {1}", numero1, numero2);
             Console.ReadKey();
+
             // Passagem por referência
             Teste teste1 = new Teste();
             teste1.Quantidade = 3;
             Teste teste2 = teste1;
             teste1.Quantidade = 5;
             Console.WriteLine("teste1.Quantidade = {0}, teste2.Quantidade = {1}", teste1.Quantidade, teste2.Quantidade);
+            Console.ReadKey();*/
+
+            // Simulação do erro NullException
+            Teste teste1 /*= new Teste()*/;
+            teste1.Quantidade = 3;
+            //teste1.testePai = new Teste();
+            teste1.testePai.Quantidade = 6;
+            Console.WriteLine("Quantidade é {0}", teste1.Quantidade);
+            Console.WriteLine("Quantidade do teste pai {0}", teste1.testePai.Quantidade);
             Console.ReadKey();
         }
     }
@@ -29,6 +39,7 @@ namespace ValueTypeVsReferenceType
     class Teste
     {
         public int Quantidade;
+        public Teste testePai;
     }
 
 }
