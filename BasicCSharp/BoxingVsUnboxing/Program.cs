0f5a014ec337a039b1be4202835ea70aa0cd1cca
raphaellpro <826.xxxxx@gmail.com>
2025-05-30 22:31:48 -0300

    feat: BoxingVsUnboxing

--- BasicCSharp/BoxingVsUnboxing/Program.cs
+++ BasicCSharp/BoxingVsUnboxing/Program.cs
@@ -10,6 +10,16 @@ namespace BoxingVsUnboxing
     {
         static void Main(string[] args)
         {
+            // Boxing
+            int inteiro1 = 2;
+            object objeto1 = inteiro1;
+            Console.WriteLine("object = {0}", objeto1);
+            Console.ReadKey();
+
+            // Unboxing
+            int inteiro2 = (int)objeto1;
+            Console.WriteLine("inteiro2 = {0}", inteiro2);
+            Console.ReadKey();
         }
     }
 }
